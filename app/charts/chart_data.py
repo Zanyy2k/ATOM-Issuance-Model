c601704a38b65344b61f68902687de3c1a2f4dd2
Zongdong Yu <yzd@Zongdongs-MacBook-Pro.local>
2024-02-23 23:40:49 +0800

    added the full logic into the webapp, with table shown and graphs in ui

--- app/charts/chart_data.py
+++ app/charts/chart_data.py
@@ -1,17 +1,26 @@
 import plotly.express as px
 import pandas as pd
-import random
 
-def get_chart_data():
-    # Generate random chart data for demonstration
-    data = {'x': range(10), 'y': [random.randint(1, 10) for _ in range(10)]}
-    df = pd.DataFrame(data)
-    
-    # Create a scatter plot
-    fig = px.scatter(df, x='x', y='y', title='Dynamic Chart')
+def generate_issued_chart(df):
+    # Generate Issued ATOM chart
+    fig = px.line(df, x='Month', y=['Inflow Issued ATOM (new)', 'Issued ATOM (old)'],
+                labels={'value': 'Value', 'variable': 'Category'},
+                title='Issued ATOM',
+                template='plotly_white')
 
     # Convert the Plotly figure to JSON for rendering in the template
     chart_data = fig.to_json()
-    print(chart_data)
+    return chart_data
 
+
+def generate_cumm_issued_chart(df):
+    # Generate Cumulatively Issued ATOM
+    fig = px.line(df, x='Month', y=['Total Cumulatively Issued ATOM (new)', 'Cumulatively Issued ATOM (old)'],
+                labels={'value': 'Value', 'variable': 'Category'},
+                title='Cumulatively Issued ATOM',
+                template='plotly_white',
+                line_dash_sequence=['solid', 'dot'])
+
+    # Convert the Plotly figure to JSON for rendering in the template
+    chart_data = fig.to_json()
     return chart_data
--- app/main.py
+++ app/main.py
@@ -1,11 +1,102 @@
 from flask import Flask, render_template, request
-from charts.chart_data import get_chart_data
+from charts.chart_data import generate_issued_chart, generate_cumm_issued_chart
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', data=None)
+
+
+# Outflow - Security Subsidy
+def calculate_outflow_security_subsidy(month, security_subsidy_termination, current_security_subsidy, security_subsidy_decay, distrbution_module_pct_to_community_pool):
+    if month > security_subsidy_termination : 
+        return 0
+    else: 
+        return(current_security_subsidy * pow((1-security_subsidy_decay/100),month)*(1-distrbution_module_pct_to_community_pool/100))
+    
+
+# Outflow - Distribution to Community Pool
+def calculate_outflow_distribution_community_pool(month, security_subsidy_termination, current_security_subsidy, security_subsidy_decay, distrbution_module_pct_to_community_pool):
+    if month > security_subsidy_termination : 
+        return 0
+    else: 
+        return(current_security_subsidy * pow((1-security_subsidy_decay/100),month)*(distrbution_module_pct_to_community_pool/100))
+
+
+# Inflow - Issued ATOM (new)
+def calculate_inflow_issued_atom_new(month, time_until_steady_state_issuance, issuance, issuance_reduction_amount, steady_state_issuance):
+    if month >= time_until_steady_state_issuance : 
+        return steady_state_issuance
+    else: 
+        return((issuance-200000) * pow((1-issuance_reduction_amount/100),month)+200000)
+
+
+# Total pool Balance
+def calculate_total_treasury_pool_balance(outflow_distribution_community_pool, outflow_security_subsidy, inflow_issued_atom_new):
+    total_outflow = outflow_distribution_community_pool + outflow_security_subsidy
+    total_treasury_pool_balance = inflow_issued_atom_new - total_outflow
+    return total_treasury_pool_balance
+
+
+# Total ATOM supply
+def calculate_total_atom_supply(total_atom_supply,inflow_issued_atom_new ):
+    total_atom_supply += inflow_issued_atom_new
+    return total_atom_supply
+
+
+# Old_Regime 
+def calculate_old_regime(total_atom_supply_old, issued_atom_old, cumulatively_issued_atom_old, current_security_subsidy_rate):
+    total_atom_supply_old += issued_atom_old
+    issued_atom_old = total_atom_supply_old*current_security_subsidy_rate/100/12
+    cumulatively_issued_atom_old += issued_atom_old
+    return (total_atom_supply_old, issued_atom_old, cumulatively_issued_atom_old)
+
+
+def populate_table(timeline_length, current_total_atom_supply, current_community_pool_balance, current_security_subsidy_rate, current_security_subsidy, security_subsidy_decay, security_subsidy_termination, issuance, issuance_reduction_amount, steady_state_issuance, time_until_steady_state_issuance, distrbution_module_pct_to_community_pool):
+    # Initialize lists to store data
+    data = []
+    columns = ["Month", "Outflow Security Subsidy", "Outflow Distribution to Community Pool",
+            "Inflow Issued ATOM (new)", "Total Treasury Pool Balance",
+            "Current Community Pool Balance", "Total Cumulatively Issued ATOM (new)",
+            "Total ATOM Supply", "Total ATOM Supply (old)", "Issued ATOM (old)",
+            "Cumulatively Issued ATOM (old)"]
+
+    # Call the functions
+    total_treasury_pool_balance = 0
+    current_total_comm_pool = current_community_pool_balance
+    total_cumulatively_issued_atom_new = 0
+    total_atom_supply = current_total_atom_supply
+    total_atom_supply_old = current_total_atom_supply
+    issued_atom_old = 0
+    cumulatively_issued_atom_old = issued_atom_old
+
+    for month in range(timeline_length + 1):
+        outflow_security_subsidy = calculate_outflow_security_subsidy(month, security_subsidy_termination, current_security_subsidy, security_subsidy_decay, distrbution_module_pct_to_community_pool)
+        outflow_distribution_community_pool = calculate_outflow_distribution_community_pool(month, security_subsidy_termination, current_security_subsidy, security_subsidy_decay, distrbution_module_pct_to_community_pool)
+        inflow_issued_atom_new = calculate_inflow_issued_atom_new(month, time_until_steady_state_issuance, issuance, issuance_reduction_amount, steady_state_issuance)
+        total_treasury_pool_balance += calculate_total_treasury_pool_balance(outflow_distribution_community_pool, outflow_security_subsidy, inflow_issued_atom_new)
+        current_total_comm_pool += outflow_distribution_community_pool
+        total_cumulatively_issued_atom_new += inflow_issued_atom_new
+        total_atom_supply = calculate_total_atom_supply(total_atom_supply,inflow_issued_atom_new)
+        total_atom_supply_old,issued_atom_old, cumulatively_issued_atom_old = calculate_old_regime(total_atom_supply_old, issued_atom_old, cumulatively_issued_atom_old, current_security_subsidy_rate)
+
+        # print(month, outflow_security_subsidy, outflow_distribution_community_pool, inflow_issued_atom_new, total_treasury_pool_balance, current_total_comm_pool, total_cumulatively_issued_atom_new, total_atom_supply, total_atom_supply_old,issued_atom_old, cumulatively_issued_atom_old)
+
+        # Append data for the current month to the list
+        data.append([month, outflow_security_subsidy, outflow_distribution_community_pool, inflow_issued_atom_new,
+                    total_treasury_pool_balance, current_total_comm_pool, total_cumulatively_issued_atom_new,
+                    total_atom_supply, total_atom_supply_old, issued_atom_old, cumulatively_issued_atom_old])
+
+    # Create a DataFrame from the lists
+    df = pd.DataFrame(data, columns=columns)
+
+    # Transpose the DataFrame
+    df_transposed = df.transpose()
+
+    # return the transposed DataFrame
+    return(df, df_transposed)
 
 
 @app.route('/populate', methods=['POST'])
@@ -41,8 +132,15 @@ def populate():
             'Timeline Length': timeline_length
         }
 
-    return render_template('index.html', setting_inputs = setting_inputs)
-    
+    df, data_table = populate_table(timeline_length, current_total_atom_supply, current_community_pool_balance, current_security_subsidy_rate, current_security_subsidy, security_subsidy_decay, security_subsidy_termination, issuance, issuance_reduction_amount, steady_state_issuance, time_until_steady_state_issuance, distrbution_module_pct_to_community_pool)
+
+    chart1 = generate_issued_chart(df)
+
+    chart2 = generate_cumm_issued_chart(df)
+
+    return render_template('index.html', setting_inputs = setting_inputs, data_table= data_table.to_html(), chart1=chart1, chart2=chart2 )
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
